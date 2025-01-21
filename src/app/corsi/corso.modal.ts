f24c09ec5a6e702196665d7c5ceff7ad009f7755
Ale04R <alessandro.raffo@edu.itspiemonte.it>
2025-01-21 14:34:40 +0100

    creazione del modal di corso

---
+++ src/app/corsi/corso.modal.ts
@@ -0,0 +1,9 @@
+export interface Corso {
+    id: number;
+    nome: string;
+    descrizione: string;
+    istruttore: string;
+    durata: number;
+    capacita: number;
+    preferiti: boolean;
+}
