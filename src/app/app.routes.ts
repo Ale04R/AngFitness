6a45bd90f93707b23d7096a46c7ac487211e762a
Ale04R <alessandro.raffo@edu.itspiemonte.it>
2025-01-21 14:30:28 +0100

    creazione delle rotte

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CorsiComponent } from './corsi/corsi.component';
+import { ChisiamoComponent } from './chisiamo/chisiamo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'chisiamo', component: ChisiamoComponent},
+    {path: 'corsi', component: CorsiComponent},
+];
