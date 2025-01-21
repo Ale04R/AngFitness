c9c78214144887d2a4a05970db6671fc51a7c5b3
Ale04R <alessandro.raffo@edu.itspiemonte.it>
2025-01-22 00:18:53 +0100

    creazione pagina chi siamo

--- src/app/chisiamo/chisiamo.component.ts
+++ src/app/chisiamo/chisiamo.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { IstruttoreComponent } from "./istruttore/istruttore.component";
+import { Istruttori } from "./istruttori.modal";
+import { IstruttoriService } from './istruttore.service';
 
 @Component({
   selector: 'app-chisiamo',
   standalone: true,
-  imports: [],
+  imports: [IstruttoreComponent],
   templateUrl: './chisiamo.component.html',
   styleUrl: './chisiamo.component.css'
 })
-export class ChisiamoComponent {
+export class ChisiamoComponent implements OnInit {
+  istruttori = signal<Istruttori[] | undefined>(undefined);
+  inCaricamento = signal(false);
 
+  private istruttoriService = inject(IstruttoriService);
+
+  ngOnInit(): void {
+    const subscription = this.istruttoriService.loadIstruttori()
+            .subscribe({
+              next: (resData) => {
+                this.istruttori.set(resData);
+                this.inCaricamento.set(true);
+              },
+              complete: () => {
+                this.inCaricamento.set(false);
+              }
+            })
+  }
 }
