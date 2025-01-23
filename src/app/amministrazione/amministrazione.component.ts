553215767eca511de9e888a243a7b5c9b9d49cbe
Ale04R <alessandro.raffo@edu.itspiemonte.it>
2025-01-23 11:07:45 +0100

    aggiunto filtro, ma da fixare perche funziona solo con idCorso e non mi piace =(

--- src/app/amministrazione/amministrazione.component.ts
+++ src/app/amministrazione/amministrazione.component.ts
@@ -1,4 +1,6 @@
 import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { PrenotazioneComponent } from "./prenotazione/prenotazione.component";
 import { CorsiPrenotati } from './prenotazioni.modal';
 import { PrenotazioniService } from './prenotazioni.service';
@@ -6,33 +8,43 @@ import { PrenotazioniService } from './prenotazioni.service';
 @Component({
   selector: 'app-amministrazione',
   standalone: true,
-  imports: [PrenotazioneComponent],
+  imports: [CommonModule, FormsModule, PrenotazioneComponent],
   templateUrl: './amministrazione.component.html',
   styleUrl: './amministrazione.component.css'
 })
 export class AmministrazioneComponent {
   corsiPrenotati = signal<CorsiPrenotati[] | undefined>(undefined);
   inCaricamento = signal(false);
+  filtro = signal<string>('');
 
   private destroyRef = inject(DestroyRef);
   private prenotazioniService = inject(PrenotazioniService);
 
   ngOnInit(): void {
-      
-      const subscription = this.prenotazioniService.loadCorsiPrenotati()
-          .subscribe({
-            next: (resData) => {
-              this.corsiPrenotati.set(resData);
-              this.inCaricamento.set(true);
-            },
-            complete: () => {
-              this.inCaricamento.set(false);
-            }
-          })
-      
-      this.destroyRef.onDestroy(()=>{
-            subscription.unsubscribe();
-          }
-          )
+    const subscription = this.prenotazioniService.loadCorsiPrenotati()
+      .subscribe({
+        next: (resData) => {
+          this.corsiPrenotati.set(resData);
+          this.inCaricamento.set(true);
+        },
+        complete: () => {
+          this.inCaricamento.set(false);
+        }
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  filtraPrenotazioni() {
+    const filtroValue = this.filtro();
+    if (filtroValue && this.corsiPrenotati()) {
+      this.corsiPrenotati.set(this.corsiPrenotati()!.filter(corso => corso.idCorso.includes(filtroValue)));
+    } else {
+      this.prenotazioniService.loadCorsiPrenotati().subscribe((data) => {
+        this.corsiPrenotati.set(data);
+      });
+    }
   }
 }
