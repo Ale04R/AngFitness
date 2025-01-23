fc220eb6945c1b68d54e38e9cc8eb822730ddf6a
Ale04R <alessandro.raffo@edu.itspiemonte.it>
2025-01-23 16:55:53 +0100

    eliminazione del component prenotazione e modifiche per la gestione in amministrazione

--- src/app/amministrazione/amministrazione.component.ts
+++ src/app/amministrazione/amministrazione.component.ts
@@ -1,14 +1,13 @@
 import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { PrenotazioneComponent } from "./prenotazione/prenotazione.component";
 import { CorsiPrenotati } from './prenotazioni.modal';
 import { PrenotazioniService } from './prenotazioni.service';
 
 @Component({
   selector: 'app-amministrazione',
   standalone: true,
-  imports: [CommonModule, FormsModule, PrenotazioneComponent],
+  imports: [CommonModule, FormsModule],
   templateUrl: './amministrazione.component.html',
   styleUrls: ['./amministrazione.component.css']
 })
@@ -38,14 +37,27 @@ export class AmministrazioneComponent {
   }
 
   filtraPrenotazioni() {
-    const filtroValue = this.filtro();
+    const filtroValue = this.filtro().toLowerCase();
     if (filtroValue && this.corsiPrenotati()) {
-      this.corsiPrenotati.set(this.corsiPrenotati()!.filter(corso => corso.idCorso.includes(filtroValue)));
+      this.corsiPrenotati.set(this.corsiPrenotati()!.filter(corso => {
+        const corsoNome = this.getCorsoNome(corso.idCorso).toLowerCase();
+        return corsoNome.includes(filtroValue);
+      }));
     } else {
       this.prenotazioniService.loadCorsiPrenotati().subscribe((data) => {
         this.corsiPrenotati.set(data);
       });
     }
+  }
+
+  getCorsoNome(idCorso: string): string {
+    const corso = this.prenotazioniService.getCorsoById(idCorso);
+    return corso ? corso.nome : 'Corso non trovato';
+  }
+
+  getCorsoCapMax(idCorso: string, nPrenotazioni: number): string {
+    const capMax = this.prenotazioniService.getCorsoById(idCorso);
+    return capMax ? capMax.capacitaMassima - nPrenotazioni + ' posti disponibili' : 'Capacità non trovata';
   }
 
   scrollTo(elementId: string) {
