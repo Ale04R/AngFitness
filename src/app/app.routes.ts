8eaac2986b86b9fb15b65831340da93990e9eb13
Ale04R <alessandro.raffo@edu.itspiemonte.it>
2025-01-23 09:20:49 +0100

    aggiuta rotta Amministrazione

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CorsiComponent } from './corsi/corsi.component';
 import { ChisiamoComponent } from './chisiamo/chisiamo.component';
+import { AmministrazioneComponent } from './amministrazione/amministrazione.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'chisiamo', component: ChisiamoComponent},
-    {path: 'corsi', component: CorsiComponent},
+    { path: '', component: HomeComponent },
+    { path: 'chisiamo', component: ChisiamoComponent },
+    { path: 'corsi', component: CorsiComponent },
+    { path: 'amministrazione', component: AmministrazioneComponent }
 ];
