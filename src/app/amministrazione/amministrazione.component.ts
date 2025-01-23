f5123a0c0f95141453e9cb95144d20614d4a6468
Ale04R <alessandro.raffo@edu.itspiemonte.it>
2025-01-23 16:51:00 +0100

    correzione e implemento per la parte di visualizzazione dei corsi

--- src/app/amministrazione/amministrazione.component.ts
+++ src/app/amministrazione/amministrazione.component.ts
@@ -47,4 +47,20 @@ export class AmministrazioneComponent {
       });
     }
   }
+
+  scrollTo(elementId: string) {
+    const element = document.getElementById(elementId);
+    if (element) {
+      const offset = -80; // Altezza dello spazio aggiunto
+      const bodyRect = document.body.getBoundingClientRect().top;
+      const elementRect = element.getBoundingClientRect().top;
+      const elementPosition = elementRect - bodyRect;
+      const offsetPosition = elementPosition + offset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    }
+  }
 }
